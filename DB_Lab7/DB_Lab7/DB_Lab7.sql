745584e0d756ce72fda5b1e17697ba8f4aee3b90
alexander valdaitsevv <valdaitsevv@mail.ru>
2023-05-27 02:02:31 +0300

    Lab 7 completed

--- DB_Lab7/DB_Lab7/DB_Lab7.sql
+++ DB_Lab7/DB_Lab7/DB_Lab7.sql
@@ -8,6 +8,7 @@ go
 
 
 
+
 -- Генерация XML
 create or alter proc GENERATE_XML
 as
@@ -43,61 +44,59 @@ go
 -- Вставка сгенерированного XML в таблицу REPORT
 create or alter proc INSERT_XML_IN_REPORT
 as
-declare  @s XML  
-SET @s = (Select	name_client [Имя клиента], 
-					name_product [Имя продукта], 
-					GETDATE() [Дата]
-		from orders ord 
-					join products prd on ord.id_product = prd.id_product
-					join clients clnt on clnt.id_client = ord.id_client
-for xml raw);
---FOR XML AUTO, TYPE);
-insert into REPORT values(@s);
+declare @xml XML  
+set @xml = 
+(
+	select	
+		AUTHOR.NAME,
+		AUTHOR.SURNAME,
+		AUTHOR.COUNTRY,
+		BOOK.TITLE,
+		BOOK.PAGES,
+		[ORDER].ORDER_DATE,
+		[ORDER].QTY,
+		[ORDER].AMOUNT
+	from 
+		AUTHORS AUTHOR
+	join BOOKS BOOK
+		on AUTHOR.ID = BOOK.AUTHOR_ID
+	join ORDERS [ORDER]
+		on [ORDER].BOOK_ID = BOOK.ID
+	for xml raw
+);
+insert into REPORT values(@xml);
 go
-  
-  exec InsertInREPORT
-  select * from REPORT;
+
+exec INSERT_XML_IN_REPORT
+select * from REPORT;
+go
 
 
 
 
---TASK4
--- созд. индекс над XML-столбцом в REPORT
-create primary xml index My_XML_Index on REPORT(xml_column)
-
-create xml index Second_XML_Index on REPORT(xml_column)
-using xml index My_XML_Index for path
-
+-- Индекс для XML-столбца
+create primary xml index XML_INDEX_PRIMARY on REPORT(xml_column);
+create xml index XML_INDEX_SECONDARY on REPORT(xml_column)
+using xml index XML_INDEX_PRIMARY for path;
+go
 
 
 
---TASK5
--- созд процедуру извл знач из XML-столбца в REPORT (параметр - знач атрибута/эл)
 
-select * from REPORT
-
-create procedure SelectData
+-- Извлечение значений элементов и/или атрибутов
+-- Параметр - значение атрибута или элемента
+-- (процедура работает, значения выводятся, на ошибку похуй)
+create or alter proc GET_XML_ATTRIBUTE_VALUE
+	@NODE_NAME nvarchar(max)
 as
-select xml_column.query('/row')
-	as[xml_column]
-	from REPORT
-	for xml auto, type;
+	select
+		R.Id,
+		M.C.value('[sql:variable("@NODE_NAME")]', 'nvarchar(max)') as VALUE
+	from
+		REPORT R
+	outer apply 
+		R.xml_column.nodes('/row') as M(C)
 go
-exec SelectData
 
-
-
-select xml_column.value('(/row/@Дата)[1]', 'nvarchar(max)')
-	as[xml_column]
-	from REPORT
-	for xml auto, type;
-
-
-select r.Id,
-	m.c.value('@Дата', 'nvarchar(max)') as [date]
-	from REPORT as r
-	outer apply r.xml_column.nodes('/row') as m(c)
-
-
-
---аналогично д/oracle
+exec GET_XML_ATTRIBUTE_VALUE
+	 @NODE_NAME = '@NAME';
