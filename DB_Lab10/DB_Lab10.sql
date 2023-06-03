adca90edb0941b2762a69ca44467d7fe7df8ebfd
alexander valdaitsevv <valdaitsevv@mail.ru>
2023-06-03 09:20:48 +0300

    Lab 10 full

--- DB_Lab10/DB_Lab10.sql
+++ DB_Lab10/DB_Lab10.sql
@@ -2,7 +2,7 @@ use Sublish;
 
 --2. Создать логин, роль, пользователя, привилегии
 	create login Lab10_Login with password = '1111';		--логин
-	create user Lab10_User for login Lab10_Login;			--юзер, привяз. к логину
+	create user Lab10_User for login Lab10_Login;			--юзер, привязанный к логину
 	create user DefaultUser without login;					--юзер
 	create role DefaultRole;								--роль
 
@@ -80,153 +80,158 @@ use Sublish;
 
 ---- А У Д И Т    Б А З Ы    Д А Н Н Ы Х ----
 
---7. Создать объекты аудита БД + спец + запуск + журнал
-	create database audit specification DatabaseAuditSpecification1
+--7. Создать объекты аудита базы данных и спецификацию
+	create database audit specification MySpecSublish
 		for server audit MyAudit
-		add (insert on [Sklad].dbo.Products by dbo)
-		with (state=on);
+		add (insert on Sublish.dbo.AUTHORS by dbo)
+		with (state = on);
 
-	select * from [Sklad].dbo.Products;
-	delete from [Sklad].dbo.Products where name_product='Хурма';
-	insert into [Sklad].dbo.Products(name_product, kol) values ('Хурма', 28);
+	select * from Sublish.dbo.AUTHORS;
 
-	select statement from fn_get_audit_file('D:\3\БД\10\*', null, null);	
+	insert into Sublish.dbo.AUTHORS(ID, NAME, SURNAME, COUNTRY, DATE_OF_BIRTH) 
+	values (NEWID(), N'Чел', N'Рофлик', N'США', '12-20-1999');
+
+	delete from Sublish.dbo.AUTHORS
+	where SURNAME = N'Рофлик';
+
+	select statement 
+	from fn_get_audit_file('C:\Users\valda\source\repos\semester#6\DB\DB_Lab10\MyAudit\*', null, null);	
 
 
 --10. Остановить адуит БД и сервера
-	alter server audit MyAudit with (state=off);
-	alter server audit PAudit with (state=off);
-	alter server audit AAudit with (state=off);
+	use master;
+	alter server audit MyAudit with (state = off);
+	alter server audit PAudit  with (state = off);
+	alter server audit AAudit  with (state = off);
 
 
 ---- Ш И Ф Р О В А Н И Е ----
 
---11. Создать ассим. ключ шифрования
-	use Sklad;
-	create asymmetric key SampleAKey
+--11. Создать ассиметричный ключ
+	use Sublish;
+	create asymmetric key MyAsymmetricKey
 		with algorithm = rsa_2048
-		encryption by password = 'Pas45!!~~';
+		encryption by password = '1111';
 
---12. Зашифр и расш- данные при пом ключа
+--12. Зашифрование и расшифрование при помощи ключа
 	declare @plaintext nvarchar(21);
 	declare @ciphertext nvarchar (256);
 
-	set @plaintext = 'this is a sample text';
-	print @plaintext;
+	set @plaintext = 'hello everyone!';
+	print N'Открытый текст:        ' + @plaintext;
 
-	set @ciphertext = EncryptByAsymKey(AsymKey_ID('SampleAKey'), @plaintext);
-	print @ciphertext;
+	set @ciphertext = EncryptByAsymKey(AsymKey_ID('MyAsymmetricKey'), @plaintext);
+	print N'Зашифрованный текст:   ' + @ciphertext;
 
-	set @plaintext = DecryptByAsymKey(AsymKey_ID('SampleAKey'), @ciphertext, N'Pas45!!~~');
-	print @plaintext;
+	set @plaintext = DecryptByAsymKey(AsymKey_ID('MyAsymmetricKey'), @ciphertext, N'1111');
+	print N'Расшифрованный текст:  ' + @plaintext;
 
 
 --13. Создать сертификат
-	create certificate SampleCert
-		encryption by password = N'pa$$W0RD'
-		with subject = N'Sample Certificate',
-		expiry_date = N'31/10/2022';
+	create certificate MyCertificate
+		encryption by password = N'1111'
+		with subject = N'My Certificate',
+		expiry_date = N'10-10-2024';
 
 
---14. Зашифр и расшиф данные при пом. сертификата.
+--14. Зашифрование и расшифрование при помощи сертификата
 	declare @plain_text nvarchar(58);
-	set @plain_text = 'this is certificate encryption text';
-	print @plain_text;
+	set @plain_text = 'hello pacani';
+	print N'Открытый текст:        ' + @plain_text;
 
 	declare @cipher_text nvarchar(256);
-	set @cipher_text = EncryptByCert(Cert_ID('SampleCert'), @plain_text);
-	print @cipher_text;
+	set @cipher_text = EncryptByCert(Cert_ID('MyCertificate'), @plain_text);
+	print N'Зашифрованный текст:   ' + @cipher_text;
 
-	set @plain_text = CAST(DecryptByCert(Cert_ID('SampleCert'), @cipher_text, N'pa$$W0RD') as nvarchar(58));
-	print @plain_text;
+	set @plain_text = CAST(DecryptByCert(Cert_ID('MyCertificate'), @cipher_text, N'1111') as nvarchar(58));
+	print N'Расшифрованный текст:  ' + @plain_text;
 	
 
---15. Создать симм. ключ шифрования
-	create symmetric key SKey
+--15. Создать симметричный ключ 
+	create symmetric key MySymmetricKey
 	with algorithm = AES_256
-	encryption by password = N'PA$$W0RD';
+	encryption by password = N'1111';
 
-	open symmetric key SKey
-	decryption by password = N'PA$$W0RD';
-
-	create symmetric key SData
+	create symmetric key MyData
 	with algorithm = AES_256
-	encryption by symmetric key SKey;
-
-	open symmetric key SData
-	decryption by symmetric key SKey;
+	encryption by symmetric key MySymmetricKey;
 
 
---16. Зашифр и расшифр данные при пом. ключа
+--16. Зашифрование и расшифрование при помощи ключа
+	open symmetric key MySymmetricKey
+	decryption by password = N'1111';
+
+	open symmetric key MyData
+	decryption by symmetric key MySymmetricKey;
 	declare @plain_tex nvarchar(512);
-	set @plain_tex = 'open the symmetric key with which to encrypt the data';
-	print @plain_tex;
+	set @plain_text = 'hello everyone again';
+	print N'Открытый текст:        ' + @plain_text;
 
 	declare @cipher_tex nvarchar(1024);
-	set @cipher_tex = EncryptByKey(Key_GUID('SData'), @plain_tex);
-	print @cipher_tex;
+	set @cipher_tex = EncryptByKey(Key_GUID('MyData'), @plain_text);
+	print N'Зашифрованный текст:   ' + @cipher_tex;
 
-	set @plain_tex = CAST(DecryptByKey(@cipher_tex) as nvarchar(512));
-	print @plain_tex;
+	set @plain_text = CAST(DecryptByKey(@cipher_tex) as nvarchar(512));
+	print N'Расшифрованный текст:  ' + @plain_text;
 
-	close symmetric key SData;
-	close symmetric key SKey;
+	close symmetric key MyData;
+	close symmetric key MySymmetricKey;
 
---17. Продем. прозрачное шифрование БД
+
+--17. Прозрачное шифрование БД
 	use master;
-	create master key encryption by password = 'p@$$wOrd';
+	create master key encryption by password = '1111';
 	
-	create certificate LabCert
-		with subject = 'certificate to encrypt Lab10 DB ', 
-		expiry_date = '31/10/2020';
+	create certificate MyCertificateTDE
+		with subject = 'TDE Certificate', 
+		expiry_date = '10-10-2024';
 
 	
-	use Sklad;
+	use Sublish;
 	create database encryption key
 	with algorithm = AES_256
-	encryption by server certificate LabCert;
+	encryption by server certificate MyCertificateTDE;
 	go
 
-	alter database Sklad
+	alter database Sublish
 	set encryption on;
 	go
 
-	--удалить шифрование из БД
-	alter database Sklad 
+	-- удалить шифрование из БД
+	alter database Sublish 
 	set encryption off;
 	go
 
 
---18. Продем. хеширование (MD2, MD4, MD5, SHA1, SHA2)
-	use Sklad
-	--
-	select HashBytes('SHA1', 'open the symmetric key with which to encrypt the data');
-	select HashBytes('MD4', 'open the symmetric key with which to encrypt the data');
+--18. Хеширование (MD2, MD4, MD5, SHA1, SHA2)
+	use Sublish
+	select HashBytes('SHA1', 'hello i need to be hashed') SHA1;
+	select HashBytes('MD5', 'and my son need to be hashed too') MD5;
 
 
---19. Продем применение ЭЦП при помощи сертификата.
+--19. ЭЦП при помощи сертификата
 
 	--подписывает текст сертификатом и возвращает подпись
-	select * from sys. certificates;
-	select SIGNBYCERT(258, N'univer', N'pa$$W0RD') as ЭЦП;	--сертификат	
+	select * from sys.certificates;
+	select SIGNBYCERT(256, N'Sublish', N'1111') as ЭЦП;	--сертификат	
 	--0 - изменены, 1 - не изменены
-	select VERIFYSIGNEDBYCERT(258, 'univer', 0x0106000000000009010000004D228C4307CD964BC78E7566920B92C179801A42);
+	select VERIFYSIGNEDBYCERT(256, 'Sublish', 0x0106000000000009010000004D228C4307CD964BC78E7566920B92C179801A42) Verified;
 	
-	select * from sys. asymmetric_keys;
-	select SIGNBYASymKey(256, N'univer', N'Pas45!!~~') as ЭЦП;	--ас.ключ
-	select VERIFYSIGNEDBYASYMKEY(256, N'univer', 0x0602000000240000525341310004000001000100272736AD6E5F9586BAC2D531EABC3ACC666C2F8EC879FA94F8F7B0327D2FF2ED523448F83C3D5C5DD2DFC7BC99C5286B2C125117BF5CBE242B9D41750732B2BDFFE649C6EFB8E5526D526FDD130095ECDB7BF210809C6CDAD8824FAA9AC0310AC3CBA2AA0523567B2DFA7FE250B30FACBD62D4EC99B94AC47C7D3B28F1F6E4C8);
+	select * from sys.asymmetric_keys;
+	select SIGNBYASYMKEY(256, N'Sublish', N'1111') as ЭЦП;	--ассиметричный ключ
+	select VERIFYSIGNEDBYASYMKEY(256, N'Sublish', 0x0100050204000000293841455AFFD0103407CAE23C72BFA10FF5568F794E60D94115642DF6E1086BB5F63835A43E603ADE2B2D774F24AB0B0618FDED34983447DE49D99382DF633781166942BA18CAF0ED4A3E875724E84E844093E4A439C0486C349FCA3506BED3C74BBFC25E5E199C75C96990513E5A295B00BE39A33DB5108C1E69CDF545EE3E73C1B71099CB214B98929FFAB32E2839BAC05B3E4AE585C62D0C19773069B5E797B06F5D21BDF5BFB5747F636F80E79AE954CFB20F2F7DA433A8BCBDFB9F7F60951C298890DE909E30452984FE974696BBB7B0C8B86F8D0FB6414F473A322C7FC84C0F9E8924256D0D1FC1A8E4749068BF6622D9C0AABC497AE88FADFDA684C1) Verified;
 
 
---20. Сделать резервную копию необходимых ключей и сертификатов.
-	backup certificate SampleCert
-	to file = N'D:\3\БД\10\backup\BackupSampleCert.cer'
+--20. Сделать резервную копию ключей и сертификатов
+	backup certificate MyCertificate
+	to file = N'C:\Users\valda\source\repos\semester#6\DB\DB_Lab10\Backup\certificateBackup.cer'
 		with private key(
-			file = N'D:\3\БД\10\backup\BackupSampleCert.pvk',
-			encryption by password = N'pa$$W0RD',
-			decryption by password = N'pa$$W0RD');
+			file = N'C:\Users\valda\source\repos\semester#6\DB\DB_Lab10\Backup\certificateBackup.pvk',
+			encryption by password = N'1111',
+			decryption by password = N'1111');
 
 	use master;
-	BACKUP MASTER KEY TO FILE = 'D:\3\БД\10\backup\BackupMasterKey.key' 
-			ENCRYPTION BY PASSWORD = 'p@$$wOrd';
+	BACKUP MASTER KEY TO FILE = 'C:\Users\valda\source\repos\semester#6\DB\DB_Lab10\Backup\masterKeyBackup.key' 
+			ENCRYPTION BY PASSWORD = '1111';
 
 		
